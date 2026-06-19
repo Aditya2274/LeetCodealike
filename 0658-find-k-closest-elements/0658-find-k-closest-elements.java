e40aa1a965b760725aad051f6c8ae4730073d71f
ReZero <149367552+Aditya2274@users.noreply.github.com>
2026-06-19 14:22:49 +0530

    Time: 5 ms (58.13%), Space: 47.9 MB (50.54%) - LeetHub - Time: O(log(n) + k), Space: O(k)

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,45 +1,23 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int n;
-        int diff;
-        Pair(int n,int diff){
-            this.n=n;
-            this.diff=diff;
-        }
-        public int compareTo(Pair other){
-            if(other.diff!=diff){
-                return diff-other.diff;
+        public List<Integer> findClosestElements(int[] arr, int k, int x) {
+            int lo=0,hi=arr.length-k;
+            while(lo<hi){
+                int mid=lo+(hi-lo)/2;
+                if(arr[mid+k]-x<x-arr[mid]){
+                    lo=mid+1;
+                }
+                else{
+                    hi=mid;
+                }
             }
-            else{
-                return n-other.n;
+            List<Integer> ans=new ArrayList<>();
+            for(int i=lo;i<lo+k;i++){
+                ans.add(arr[i]);
             }
+            return ans;
         }
-    }
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int n=arr.length;
-        PriorityQueue<Pair> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<n;i++){
-            pq.add(new Pair(arr[i],Math.abs(arr[i]-x)));
-            if(pq.size()>k) pq.poll();
-        }
-        List<Integer> arr1=new ArrayList<>();
-        for(int i=0;i<k;i++){
-            arr1.add(pq.poll().n);
-        }
-        Collections.sort(arr1);
-        return arr1;
-    }
 }
-
-
-
-
-
-
-
-
-// this.value - other.value → ascending order → PriorityQueue acts as a min-heap.
-// other.value - this.value → descending order → PriorityQueue acts as a max-heap.
+//Try it using lambda functions as well
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
