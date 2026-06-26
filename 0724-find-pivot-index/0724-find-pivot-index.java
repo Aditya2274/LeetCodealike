3cf6924e935c814b1b480b3c2126aa183521f72b
ReZero <149367552+Aditya2274@users.noreply.github.com>
2026-06-26 14:32:39 +0530

    Time: 1 ms (98.58%), Space: 47.3 MB (77.16%) - LeetHub - Time: O(n), Space: O(1)

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,22 +1,18 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-       int n=nums.length;
-       for(int i=0;i<n;i++){
-         int left=0,right=0;
-         for(int j=0;j<i;j++){
-            left+=nums[j];
-         }
-         for(int j=i+1;j<n;j++){
-            right+=nums[j];
-         }
-         if(left==right){
-            return i;
-         }
-       }
-       return -1;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        for(int i=0;i<n;i++){
+            int left=0;
+            if(i>0) left=nums[i-1];
+            int right=nums[n-1]-nums[i];
+            if(left==right) return i;
+        }
+        return -1;
     }
 }
-//Brute force
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
