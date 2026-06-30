5ecca475bde2fe7fe5fb72c05563dff5c0f07389
ReZero <149367552+Aditya2274@users.noreply.github.com>
2026-07-01 01:21:46 +0530

    Time: 2612 ms (5.06%), Space: 50.9 MB (63.73%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == goal)
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
