140ce7b66de6830db15df268bdef8902dc04d3ae
ReZero <149367552+Aditya2274@users.noreply.github.com>
2026-07-01 12:36:11 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.33%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int lo=0,hi=nums.length-1;int mid=0;
+        while(lo<=hi){
+            mid=lo+(hi-lo)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[lo]<=nums[mid]){
+                if(nums[lo] <= target && target < nums[mid]){
+                    hi=mid-1;
+                }
+                else lo=mid+1;
+            }
+            else{
+                if(nums[mid] < target && target <=nums[hi]){
+                    lo=mid+1;
+                }
+                else{
+                    hi=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
