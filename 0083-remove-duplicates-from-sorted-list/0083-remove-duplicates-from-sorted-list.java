b7c092a8a161e3e509265b57f90ed987bd13768c
ReZero <149367552+Aditya2274@users.noreply.github.com>
2026-07-07 11:48:22 +0530

    Time: 0 ms (100%), Space: 44.5 MB (99.84%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null) return head;
+        ListNode a=head;
+        ListNode b=head;
+        while( b!=null  ){
+            if(a.val==b.val){
+                b=b.next;
+            }
+            else{
+            a.next=b;
+            a=b;
+            }
+        }
+        a.next=b;// OR a.next=null, because will point to the last unique element
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
