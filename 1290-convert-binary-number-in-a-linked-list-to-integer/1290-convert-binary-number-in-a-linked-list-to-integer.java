071f336da83209eb4915793ad8aeefb1142b7500
ReZero <149367552+Aditya2274@users.noreply.github.com>
2026-07-07 11:18:06 +0530

    Time: 0 ms (100%), Space: 42.9 MB (55.99%) - LeetHub - Time: O(, Space: O(?)

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int num=0; ListNode temp=head;int c=0;
+        while(temp!=null){
+            temp=temp.next;
+            c++;
+        }
+        temp=head;
+        while(temp!=null){
+            num+=temp.val*Math.pow(2,--c);
+            temp=temp.next;
+        }
+        return num;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
